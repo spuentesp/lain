3e5cab3134e468045fc047637230d69ccbc642c9
spuentesp <spuentesp@gmail.com>
2026-05-05 18:22:24 -0400

    chore: update Homebrew formula with SHA256 hashes for v0.2.0

--- Formula/lain.rb
+++ Formula/lain.rb
@@ -9,19 +9,21 @@ class Lain < Formula
   on_macos do
     on_arm do
       url "https://github.com/spuentesp/lain/releases/download/v0.2.0/lain-0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "UPDATE_SHA256_ARM_MAC"
+      sha256 "ee38fbde32a2908bc58c9d2a9d0b9751e305f22f46b94aa1ae41941b76319ab9"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/spuentesp/lain/releases/download/v0.2.0/lain-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "UPDATE_SHA256_INTEL_LINUX"
+      sha256 "4e76d63994545d1305c95ea46b5d63da3daafc563bd7ce4458b238f7e4144b2e"
     end
+  end
 
-    on_arm do
-      url "https://github.com/spuentesp/lain/releases/download/v0.2.0/lain-0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "UPDATE_SHA256_ARM_LINUX"
+  on_windows do
+    on_intel do
+      url "https://github.com/spuentesp/lain/releases/download/v0.2.0/lain-0.2.0-x86_64-pc-windows-msvc.tar.gz"
+      sha256 "6138f21282510f571237301ecb8c2d995a3494ae470c23f544e539a6122645c7"
     end
   end
 
